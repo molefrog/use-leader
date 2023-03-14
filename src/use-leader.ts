f2fea7d79028357a2b3cffb90613fd32a893437f
Alexey Taktarov <molefrog@gmail.com>
2023-03-14 14:10:56 +0200

    First attempt to handle multiple instances within a page.

--- src/use-leader.ts
+++ src/use-leader.ts
@@ -1,25 +1,84 @@
-import { useState, useSyncExternalStore } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+  useSyncExternalStore,
+} from "react";
 import { nanoid } from "nanoid";
 
 import Bully from "./bully.ts";
+import { ID } from "./protocol.ts";
 
-const useLeader = () => {
-  const [bully] = useState(() => {
-    const nodeId = nanoid();
+interface RefCounter {
+  instance: Bully;
+  refs: Set<symbol>;
+}
 
-    const instance = new Bully(nodeId);
-    instance.live();
+const ManagerCtx = createContext<
+  Record<ID | "default", RefCounter>
+>({});
 
-    return instance;
+const useInstanceManager = () => useContext(ManagerCtx);
+
+interface Options {
+  id?: ID;
+}
+
+export const useLeaderInstance = (options?: Options): Bully => {
+  const manager = useInstanceManager();
+
+  const hookRef = useRef(Symbol());
+  const lookupRef = useRef<ID>();
+
+  const [instance] = useState<Bully>(() => {
+    const lookup = lookupRef.current = options?.id || "default";
+
+    if (manager[lookup]) {
+      manager[lookup].refs.add(hookRef.current);
+    } else {
+      // set up and run a new instance
+      const instance = new Bully(options?.id || nanoid());
+      manager[lookup] = { instance, refs: new Set([hookRef.current]) };
+
+      instance.live();
+    }
+
+    return manager[lookup].instance;
   });
 
-  const isLeader = useSyncExternalStore(
-    (fn) => bully.events.on("elected", fn),
-    () => bully.isLeader(),
-    () => true,
-  );
+  useEffect(() => {
+    return () => {
+      const key = lookupRef.current;
 
-  return isLeader;
+      if (key) {
+        manager[key].refs.delete(hookRef.current);
+
+        // no one is using it anymore, cleanup
+        if (manager[key].refs.size === 0) {
+          delete manager[key];
+          instance.destroy();
+        }
+      }
+    };
+  }, []);
+
+  return instance;
+};
+
+/**
+ * @param options additional options to customize leader detection
+ * @returns true if current node is a leader
+ */
+const useLeader = (options?: Options): boolean => {
+  const instance = useLeaderInstance(options);
+
+  return useSyncExternalStore(
+    (fn) => instance.events.on("elected", fn),
+    () => instance.isLeader(),
+    () => true, // in SSR mode, always assume we're leading
+  );
 };
 
 export default useLeader;
--- web/index.tsx
+++ web/index.tsx
@@ -1,13 +1,36 @@
 /** @jsx h */
+import { nanoid } from "nanoid";
 import { render, h } from "preact";
-import useLeader from "../src/use-leader.ts";
+import { useState } from "preact/hooks";
+import useLeader, { useLeaderInstance } from "../src/use-leader.ts";
+
+const LeaderDetection = () => {
+  const ref = useLeaderInstance();
+  const isLeader = useLeader();
+
+  const currentLeader = ref.leader || "Election";
+
+  return (
+    <div style={{ marginBottom: "16px" }}>
+      <p>
+        Am I a leader? <code>{String(isLeader)}</code>
+      </p>
+      <small>
+        Node ID: <code>{ref.id}</code>
+        <br /> Leader ID: <code>{ref.leader}</code>
+      </small>
+    </div>
+  );
+};
 
 const Foo = () => {
-  const isLeader = useLeader();
+  const [mount, setMount] = useState<boolean>(true);
 
   return (
     <div>
-      Am I a leader? <code>{String(isLeader)}</code>
+      <button onClick={() => setMount((x) => !x)}>{mount ? "stop" : "start"}</button>
+      {mount && <LeaderDetection />}
+      {mount && <LeaderDetection />}
     </div>
   );
 };
