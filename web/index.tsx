208ef76dfaed287f4ad6a2bd1ca055d04b1b8bb7
Alexey Taktarov <molefrog@gmail.com>
2023-03-15 18:17:47 +0200

    Website with a simple demo.

--- web/index.tsx
+++ web/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
-import { nanoid } from "nanoid";
 import { render, h } from "preact";
 import { useState } from "preact/hooks";
+
 import useLeader, { useIsLeader } from "../src/use-leader.ts";
 
 const LeaderDetection = () => {
@@ -9,28 +9,56 @@ const LeaderDetection = () => {
   const currentLeader = leaderId || "Election";
 
   return (
-    <div style={{ marginBottom: "16px" }}>
-      <p>
-        Am I a leader? <code>{String(isLeader)}</code>
-      </p>
-      <small>
-        Node ID: <code>{id}</code>
-        <br /> Leader ID: <code>{leaderId}</code>
-      </small>
+    <div>
+      <pre>
+        {"\n\n"}
+        {">>>Am I a Leader? "}
+        {isLeader ? <mark>YES!</mark> : "NO"}
+        {"\n\n"}
+        {`Node ID............${id}\n`}
+        {`Leader ID..........${currentLeader}\n`}
+      </pre>
     </div>
   );
 };
 
-const Foo = () => {
-  const [mount, setMount] = useState<boolean>(true);
+const IsLeader = (props) => {
+  const isLeader = useIsLeader();
+  return isLeader ? props.yes : props.no;
+};
+
+const App = () => {
+  const [demoActive, setDemoActive] = useState<boolean>(true);
 
   return (
-    <div>
-      <button onClick={() => setMount((x) => !x)}>{mount ? "stop" : "start"}</button>
-      {mount && <LeaderDetection />}
-      {mount && <LeaderDetection />}
-    </div>
+    <main>
+      <h1>useLeader()</h1>
+      <h2>
+        <pre>
+          {"       React hook for optimistic        \n"}
+          {"       cross-tab leader election        "}
+        </pre>
+      </h2>
+
+      <pre>
+        {"\n              "}
+        <a href="https://github.com/molefrog/use-leader">{"-> GitHub"}</a>
+      </pre>
+
+      <pre>
+        {"\n\n"}
+        {"const {isLeader} = useLeader() // "}
+        {demoActive ? <IsLeader yes="true" no="false"></IsLeader> : "false"}
+
+        {"\n\n"}
+        <a href="javascript:void(0)" onClick={() => setDemoActive((x) => !x)}>
+          {demoActive ? "▩ Pause" : "|> Play"}
+        </a>
+      </pre>
+
+      {demoActive && <LeaderDetection />}
+    </main>
   );
 };
 
-render(<Foo />, document.body);
+render(<App />, document.body);
